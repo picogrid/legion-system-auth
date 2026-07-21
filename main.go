c9d9b86d9439083583e95d63b15a163fd3af8856
d-maltos <dylan.maltos@picogrid.com>
2026-07-21 15:34:48 -0700

    feat: add US Gov endpoint and correct Commercial URL (FDE-634)

--- main.go
+++ main.go
@@ -646,7 +646,8 @@ func ensureRedirectUriAvailable(redirectURI string) string {
 
 func selectLegionEnvironment() string {
 	envs := []struct{ Name, URL string }{
-		{"Commercial", "https://legion.picogrid.com"},
+		{"Commercial", "https://legion-prod.picogrid.com"},
+		{"US Gov", "https://api.usg.legion.picogrid.com"},
 		{"Local", "http://localhost:9876"},
 		{"Other", ""},
 	}
